aaf0af46f734ce74e2e69a6c816784ee1805242b
Bugs <abhramondal9674@gmail.com>
2025-07-09 21:59:10 +0530

    Update 50-appearance.fish
    moved from hydro to pure

--- fish/conf.d/50-appearance.fish
+++ fish/conf.d/50-appearance.fish
@@ -4,15 +4,22 @@
 #                            
 
 # Autocomplete and highlight colors 
-set -g fish_color_normal 	      brblue
+set -g fish_color_normal 	      brwhite
 set -g fish_color_autosuggestion      brblack
-set -g fish_color_command 	      brmagenta
+set -g fish_color_command 	      brgreen
 set -g fish_color_error 	      brred
-set -g fish_color_param 	      brwhite
+set -g fish_color_param 	      brcyan
 
 
 
-# Prompt (Download HYDRO prompt using fundle)
-fundle plugin 'jorgebucaran/hydro'
+# Prompt (Download pure prompt using fundle)
+fundle plugin 'pure-fish/pure'
 fundle init
 
+# Pure prompt customization
+
+# Modules
+set -g pure_show_jobs true
+
+# Colors
+set -g pure_color_primary cyan
